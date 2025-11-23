c21b723aa36ddf1c920a520b257110aef8debf52
Time-Emperor <tdulal68@gmail.com>
2025-11-23 18:15:25 +0545

    Create DataTypeInspector.java

---
+++ Week-3/DataTypeInspector.java
@@ -0,0 +1,24 @@
+public class DataTypeInspector {
+    public static void main(String[] args) {
+        
+        //intergers types
+        byte b=1;
+        short s=20;
+        int i=300;
+        long l=4000;
+        float f=10.0f;
+        double d=10.345;
+        boolean e=true;
+        char c=  'Z';
+
+        //printhing all primitive datatype
+        System.out.println("byte value:" +b);
+        System.out.println("short value:" +s);
+        System.out.println("int value:" +i);
+        System.out.println("long value:" +l);
+        System.out.println("float value:" +f);
+        System.out.println("double value:" +d);
+        System.out.println("boolean value:" +e);
+        System.out.println("char value:" +c);
+    }
+}
