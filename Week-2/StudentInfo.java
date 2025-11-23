48d914809788f5b1130fda87516a2a0268ec3d6f
Time-Emperor <tdulal68@gmail.com>
2025-11-23 18:20:37 +0545

    Create StudentInfo.java

---
+++ Week-2/StudentInfo.java
@@ -0,0 +1,13 @@
+public class StudentInfo
+{
+    public static void main(String[] args){
+             String Name="Yodin Raya";
+             int age=19;
+             double gpa=3.19;
+             
+             System.out.println("==========Student Info=====================");
+             System.out.println("Name: "+Name);
+             System.out.println("Age: "+age);
+             System.out.println("GPA: "+gpa);
+            }
+}
