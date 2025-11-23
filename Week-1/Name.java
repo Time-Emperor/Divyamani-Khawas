a8f5f479a5e7a6e3792e191f24e8d092e79c25b6
Time-Emperor <tdulal68@gmail.com>
2025-11-23 18:21:52 +0545

    Create Name.java

---
+++ Week-1/Name.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class Name here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Name
+{public static void main(String[] args){
+	System.out.println("My name iS Divyamani Khawas");
+  	}
+}
