071019105735c259c50b68f24359bcbeb342e814
Time-Emperor <tdulal68@gmail.com>
2025-11-23 18:15:32 +0545

    Create LiteralPractice.java

---
+++ Week-3/LiteralPractice.java
@@ -0,0 +1,11 @@
+public class LiteralPractice {
+   public static void main(String[] args) {
+    long num=100000L;
+    float f=2.56f;
+    char copyright='\u00A9';
+
+    System.out.println(num);
+    System.out.println(f);
+    System.out.println(copyright);
+   } 
+}
