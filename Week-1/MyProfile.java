ba1f9f497a934a7243fb89a7af2d5bada0cd2451
Time-Emperor <tdulal68@gmail.com>
2025-11-23 18:21:48 +0545

    Create MyProfile.java

---
+++ Week-1/MyProfile.java
@@ -0,0 +1,9 @@
+
+public class MyProfile
+{
+    public static void main(String[] args){
+    System.out.println("Divyamani Khawas");
+    System.out.println("I like to play Cricket");
+    System.out.println("I wanted to become a web developer");
+    }
+}
