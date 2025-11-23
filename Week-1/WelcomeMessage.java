716a587b12f65628857bb07f0610c80727dd02e3
Time-Emperor <tdulal68@gmail.com>
2025-11-23 18:21:53 +0545

    Create WelcomeMessage.java

---
+++ Week-1/WelcomeMessage.java
@@ -0,0 +1,8 @@
+public class WelcomeMessage{
+public static void main(String[] args){
+    System.out.println("Welcome to Programming");
+    System.out.println("My name is Divyamani Khawas");
+    System.out.println("I am learning Java programming");
+    System.out.println("This is my first program");
+}
+}
