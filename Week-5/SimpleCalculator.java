318cb83badf5403602de2a0b1a1ce7a8a834925b
Time Emperor <tdulal68@gmail.com>
2025-12-08 21:09:47 +0545

    Update SimpleCalculator.java

--- Week-5/SimpleCalculator.java
+++ Week-5/SimpleCalculator.java
@@ -1,59 +1,42 @@
 import java.util.Scanner;
-public class SimpleCalculator
-{
+class SimpleCalculator{
     public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
-        double result=0;
-        while(true){
-            System.out.println("Enter the first number:");
-            String input1 = scanner.nextLine();
-            if (input1.equalsIgnoreCase("stop")){
-                System.out.println("Exiting the calculator");
-                break;}
-                double num1;
-                try{
-                    num1= Double.parseDouble(input1);
-                } catch (NumberFormatException e){
-                  System.out.println("Invalid input. Please enter a valid number");
-                  continue;
-                }
-                System.out.print ("Enter the second number:");
-                String input2 = scanner.nextLine();
-                double num2;
-                try{
-                    num2= Double.parseDouble(input2);
-                }catch (NumberFormatException e){
-                 System.out.println("Invalid input for the second number. Please try again");
-                 continue;
-                }
-                System.out.print("Enter the operation(+,-,*,/):");
-                String operation = scanner.nextLine();
-                switch (operation){
-                    case "+":
-                        result = num1 + num2;
-                        System.out.println("Result:" + num1+ "+"+ num2+"="+ result);
-                        break;
-                    case "-":
-                        result=num1-num2;
-                        System.out.println("Result:" +num1 + "-" +num2+ "=" +result);
-                        break;
-                     case "*":
-                        result=num1*num2;
-                        System.out.println("Result:" +num1 + "*" +num2+ "=" +result);
-                        break;
-                    case "/":
-                        if (num2==0){
-                            System.out.println("Error: Cannot divided by zero.");
-                        } else{
-                            result = num1/num2;
-                            System.out.println("Result:" +num1 + "/" +num2+ "=" +result);
-                        }
-                        break;
-                    default:
-                        System.out.println("Error: Invalid operation. PLease use +,-,*,/");
-                        break;
-                }
+        Scanner y= new Scanner (System.in);
+        boolean ant = true;
+        while(ant){
+            System.out.println("Enter your first number: ");
+            double num1= y.nextDouble();
+            System.out.println("Enter your Second number: ");
+            double num2= y.nextDouble();
+            System.out.println("Enter the operator (+, -, *, /)");
+            String js= y.next();
+            double ans=0;
+            switch (js) {
+                case "+":
+                    ans = num1 + num2; break;
+                case "-":
+                    ans = num1 - num2; break;
+                case "*":
+                    ans = num1 * num2; break;
+                case "/":
+                    if (num2 == 0) {
+                        System.out.println("Error: Cannot divide by zero!");
+                        continue; // skip result + restart loop
+                    }
+                    ans= num1 / num2;
+                    break;
+                default : System.out.println("Invalid operation!");
+                         continue;
+                    }
+            System.out.println("Result :" +ans);
+            System.out.print("Do you want to stop? (yes/no): ");
+            String choice = y.next().toLowerCase();
+            if(!choice.equals("yes") && !choice.equals("no")){
+                System.out.println("Please enter yes or no");
             }
-            scanner.close();
+            else if (choice.equals("yes")){
+                ant = false;
+            }
         }
-    }
+   }
+}
